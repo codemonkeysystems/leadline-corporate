34614f06b6f587d8124064427d2eba0918fb3efe
Joey Scarim <joeyscarim@gmail.com>
2019-12-11 09:29:47 -0700

    update php refs

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1135,7 +1135,7 @@ function validate_and_submit_forms(form_object)
         var captcha = this_form.find("#form-captcha-img");
         if (captcha.length > 0 && this_form.is(":visible")) {
             var d = new Date().getTime();
-            captcha.replaceWith('<img id="form-captcha-img" src="assets/php/form_captcha/captcha_img.php?t='+d+'" style="display:none">');
+            captcha.replaceWith('<img id="form-captcha-img" src="https://onlinesalespro.com/joey/codemonkey/assets/php/form_captcha/captcha_img.php?t='+d+'" style="display:none">');
             this_form.find("#form-captcha").val("");
             setTimeout(function() { this_form.find("#form-captcha-img").show(); }, 500);
         }
